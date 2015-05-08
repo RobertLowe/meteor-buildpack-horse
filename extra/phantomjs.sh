0be936166aa1290f4004dbe8918c7239b05c122b
Robert Lowe <robert@iblargz.com>
2015-05-08 03:33:15 -0400

    Update phantomjs.sh

--- extra/phantomjs.sh
+++ extra/phantomjs.sh
@@ -1,9 +1,33 @@
 #!/bin/sh
 
-VERSION=1.9.7
-PHANTOM_URL="https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-$VERSION-linux-x86_64.tar.bz2"
+set -e
 
 echo "-----> Installing phantomjs."
-# Just extract the one file (the binary) and not all the examples/readme's/etc.
-FILE="phantomjs-$VERSION-linux-x86_64/bin/phantomjs"
-curl -L -sS $PHANTOM_URL -o - | tar -jxf - -C $COMPILE_DIR $FILE --strip 1
+
+BUILD_DIR=$1
+CACHE_DIR=$2
+
+# config
+VERSION="2.0.0-20141016"
+
+# Buildpack URL
+ARCHIVE_NAME=phantomjs-${VERSION}
+FILE_NAME=${ARCHIVE_NAME}-u1404-x86_64.zip
+BUILDPACK_PHANTOMJS_PACKAGE=https://github.com/bprodoehl/phantomjs/releases/download/2.0.0-20141016/${FILE_NAME}
+mkdir -p $CACHE_DIR
+if ! [ -e $CACHE_DIR/$FILE_NAME ]; then
+  echo "-----> Fetching PhantomJS ${VERSION} binaries at ${BUILDPACK_PHANTOMJS_PACKAGE}"
+  curl $BUILDPACK_PHANTOMJS_PACKAGE --verbose -L -s -o $CACHE_DIR/$FILE_NAME
+fi
+
+echo "-----> Extracting PhantomJS ${VERSION} binaries to ${BUILD_DIR}/vendor/phantomjs"
+mkdir -p $BUILD_DIR/vendor
+cd $CACHE_DIR
+unzip $FILE_NAME
+mv $ARCHIVE_NAME $BUILD_DIR/vendor/phantomjs
+chmod +x $BUILD_DIR/vendor/phantomjs/bin/phantomjs
+
+echo "-----> exporting PATH and LIBRARY_PATH"
+PROFILE_PATH="$BUILD_DIR/.profile.d/phantomjs.sh"
+mkdir -p $(dirname $PROFILE_PATH)
+echo 'export PATH="$PATH:vendor/phantomjs/bin"' >> $PROFILE_PATH
